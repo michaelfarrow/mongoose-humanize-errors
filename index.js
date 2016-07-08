42d23114bb26695fadb99a171baebf7c44259687
Mike Farrow <contact@mikefarrow.co.uk>
2016-07-08 19:58:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var messages = {
+  required: '{PATH} is required.',
+  min: '{PATH} below minimum.',
+  max: '{PATH} above maximum.',
+  enum: '{PATH} not an allowed value.',
+};
+
+module.exports = exports = function humanizeErrorsPlugin(schema, options) {
+
+  if(options && options.messages)
+    _.extend(messages, options.messages);
+
+  schema.post('validate', function(doc) {
+    if(doc.errors){
+      _.each(doc.errors, function(error, field) {
+        var properties = error.properties;
+        // If we don't have a message on the schema.
+        if(!Array.isArray(doc.schema.tree[field][error.properties.type])){
+          // If we have a message for `type`, just push the error through
+          if (messages.hasOwnProperty(properties.type)){
+            var messageFormat = messages[properties.type];
+            properties.message = messageFormat;
+            error.message = error.formatMessage(messageFormat, properties);
+          }
+        }
+      });
+    }
+
+  });
+}
